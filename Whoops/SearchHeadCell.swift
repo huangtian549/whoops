17d97ab16e38d57856341d44a122cdbdaa474f06
Li Jiatan <Lin910132@Lin910132.local>
2015-03-06 12:52:01 -0500

    SearchView(v2.0)

---
+++ Whoops/SearchHeadCell.swift
@@ -0,0 +1,26 @@
+//
+//  SearchHeadCell.swift
+//  Whoops
+//
+//  Created by Li Jiatan on 3/6/15.
+//  Copyright (c) 2015 Li Jiatan. All rights reserved.
+//
+
+import UIKit
+
+class SearchHeadCell: UITableViewCell {
+
+    @IBOutlet weak var headImg: UIImageView!    
+    @IBOutlet weak var header: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
